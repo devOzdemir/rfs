2fdc9808c2ea244486804f9f86f5de92756a5b99
devOzdemir <yunussalihozdemir@gmail.com>
2025-12-05 18:49:47 +0300

    Hepsuburada verileriyle alakali EDA calismasi yapilarak cekilen veriler taninmaya calisildi.

--- src/scrapper/scrap.py
+++ src/scrapper/scrap.py
@@ -4,10 +4,10 @@ from trendyol import scrape_trendyol
 
 def main():
     BASE_URL_HB = "https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?puan=3-max&sayfa="
-    TOTAL_PAGES_HB = 49
+    TOTAL_PAGES_HB = 1
 
     BASE_URL_TY = "https://www.trendyol.com/sr?wc=103108%2C106084&sst=MOST_RATED"
-    TOTAL_PAGES_TY = 98
+    TOTAL_PAGES_TY = 1
 
     scrape_hepsiburada(BASE_URL_HB, TOTAL_PAGES_HB)
     scrape_trendyol(BASE_URL_TY, TOTAL_PAGES_TY)
